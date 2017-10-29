c39950b0b38554123c8687cb0c7d250758edf431
FinalHopee <finalhopee@gmail.com>
2017-10-29 20:01:00 +0200

    some fixes

---
+++ mmn11/my_string.h
@@ -0,0 +1,7 @@
+void displayAnswer(int functionNumber, int functionAnswer, char *firstArgument, char *secondArgument, int thirdArgument, char *strCharAnswer);
+int my_strcmp(char *firstArgument, char *secondArgument);
+int my_strncmp(char *firstArgument, char *secondArgument, int n);
+char *my_strchr(char *firstArgument, int secondArgument);
+void OptionsMessage();
+
+#define MAX_STRING 128
